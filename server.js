34043982504f744d8005530b3ac090398517eb1e
User Guy <decoding.c.t@gmail.com>
2025-11-13 20:51:15 +0200

    adding json and bug service logger

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 import express from 'express'
 import { makeId } from './services/util.service.js'
+import { bugService } from './services/bug.service.js'
 
 
 const app = express()
 
-var bugs = [
-    {
-        _id: "abc123",
-        title: "Cannot save a new car",
-        description: "problem when clicking Save",
-        severity: 3,
-        createdAt: 1542107359454,
-    },
-    {
-        _id: '123',
-        title: "Cannot click a new car",
-        description: "problem when clicking car",
-        severity: 1,
-        createdAt: 15407359454,
-    }
-]
+// var bugs = [
+//     {
+//         _id: "abc123",
+//         title: "Cannot save a new car",
+//         description: "problem when clicking Save",
+//         severity: 3,
+//         createdAt: 1542107359454,
+//     },
+//     {
+//         _id: '123',
+//         title: "Cannot click a new car",
+//         description: "problem when clicking car",
+//         severity: 1,
+//         createdAt: 15407359454,
+//     }
+// ]
 
 // save new bug
 app.get('/api/bug/save', (req, res) => {
@@ -27,36 +28,34 @@ app.get('/api/bug/save', (req, res) => {
     const bug = { _id, title, description, severity: +severity, createdAt: Date.now() }
     // console.log(req.query)
 
-    if (bug._id) {
-        // const bugId = req.params.id
-        const idx = bugs.findIndex(bug => bug._id === _id)
-        bugs[idx] = bug
-    } else {
-        bug._id = makeId()
-        bugs.push(bug)
-    }
-    res.send(bug)
+    bugService.save(bug)
+        .then(bug => res.send(bug))
 })
 
 
 // add API for all bugs
 app.get('/api/bug', (req, res) => {
-    res.send(bugs)
+    bugService.query()
+        .then(bugs => res.send(bugs))
 })
 
 // find bug by id
 app.get('/api/bug/:id', (req, res) => {
     const bugId = req.params.id
-    const bug = bugs.find(bug => bug._id === bugId)
-    res.send(bug)
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+    // const bug = bugs.find(bug => bug._id === bugId)
+    //
 })
 
 // remove bug by id
 app.get('/api/bug/:id/remove', (req, res) => {
     const bugId = req.params.id
-    const idx = bugs.findIndex(bug => bug._id === bugId)
-    bugs.splice(idx, 1)
-    res.send('OK')
+
+    // const idx = bugs.findIndex(bug => bug._id === bugId)
+    // bugs.splice(idx, 1)
+    bugService.remove(bugId)
+        .then(() => res.send('OK'))
 })
 
 
