a9af4c9d1470a5805c7e2a6d3d4929a3df869a0c
User Guy <decoding.c.t@gmail.com>
2025-11-13 18:45:36 +0200

    app manages a bug entity

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from 'express'
 const app = express()
 
+var bug = {
+    _id: "abc123",
+    title: "Cannot save a new car",
+    description: "problem when clicking Save",
+    severity: 3,
+    createdAt: 1542107359454,
+}
+
+app.get('/api/bug', (req, res) =>
+    res.send(bug))
+
 app.get('/user', (req, res) =>
     res.send('Hello there you user!'))
+
 app.listen(3030, () => console.log('Server ready at port 3030')) 
