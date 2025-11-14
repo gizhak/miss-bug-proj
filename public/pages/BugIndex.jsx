63b9e697f0d345e90a3d6d11aa2b20ad91907840
User Guy <decoding.c.t@gmail.com>
2025-11-14 18:40:37 +0200

    adding public with full project and create service for remote

--- public/pages/BugIndex.jsx
+++ public/pages/BugIndex.jsx
@@ -1,6 +1,6 @@
 const { useState, useEffect } = React
 
-import { bugService } from '../services/bug.service.local.js'
+import { bugService } from '../services/bug.service.remote.js'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
 
 import { BugFilter } from '../cmps/BugFilter.jsx'
@@ -31,7 +31,8 @@ export function BugIndex() {
     function onAddBug() {
         const bug = {
             title: prompt('Bug title?', 'Bug ' + Date.now()),
-            severity: +prompt('Bug severity?', 3)
+            severity: +prompt('Bug severity?', 3),
+            description: +prompt('Bug description?', 3)
         }
 
         bugService.save(bug)
