f4c56e4c3dc92b9cd30f5b60f895b1a95235a37c
User Guy <decoding.c.t@gmail.com>
2025-11-22 21:35:26 +0200

    adding the service remote

--- public/services/bug.service.remote.js
+++ public/services/bug.service.remote.js
@@ -1,10 +1,10 @@
-import { utilService } from './util.service.js'
-import { storageService } from './async-storage.service.js'
+// import { utilService } from './util.service.js'
+// import { storageService } from './async-storage.service.js'
 
-const STORAGE_KEY = 'bugs'
+// const STORAGE_KEY = 'bugs'
 const BASE_URL = '/api/bug/'
 
-_createBugs()
+// _createBugs()
 
 export const bugService = {
     query,
@@ -32,49 +32,55 @@ function query(filterBy) {
 }
 
 function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
+    return storageService.get(bugId)
 }
 
 function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+    return storageService.remove(bugId)
 }
 
 function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
+    const queryParams = 'save?' +
+        `id=${bug._id || ''}&` +
+        `title=${bug.title}&` +
+        `description=${bug.description || ''}&` +
+        `severity=${bug.severity}`
+
+    console.log('Saving bug with params:', queryParams)
+
+    return axios.get(BASE_URL + queryParams)
+        .then(res => res.data)
+        .catch(err => { Promise.reject(err) })
 }
 
-function _createBugs() {
-    let bugs = utilService.loadFromStorage(STORAGE_KEY)
-    if (bugs && bugs.length > 0) return
+// function _createBugs() {
+//     let bugs = utilService.loadFromStorage(STORAGE_KEY)
+//     if (bugs && bugs.length > 0) return
 
-    bugs = [
-        {
-            title: "Infinite Loop Detected",
-            severity: 4,
-            _id: "1NF1N1T3"
-        },
-        {
-            title: "Keyboard Not Found",
-            severity: 3,
-            _id: "K3YB0RD"
-        },
-        {
-            title: "404 Coffee Not Found",
-            severity: 2,
-            _id: "C0FF33"
-        },
-        {
-            title: "Unexpected Response",
-            severity: 1,
-            _id: "G0053"
-        }
-    ]
-    utilService.saveToStorage(STORAGE_KEY, bugs)
-}
+//     bugs = [
+//         {
+//             title: "Infinite Loop Detected",
+//             severity: 4,
+//             _id: "1NF1N1T3"
+//         },
+//         {
+//             title: "Keyboard Not Found",
+//             severity: 3,
+//             _id: "K3YB0RD"
+//         },
+//         {
+//             title: "404 Coffee Not Found",
+//             severity: 2,
+//             _id: "C0FF33"
+//         },
+//         {
+//             title: "Unexpected Response",
+//             severity: 1,
+//             _id: "G0053"
+//         }
+//     ]
+//     utilService.saveToStorage(STORAGE_KEY, bugs)
+// }
 
 function getDefaultFilter() {
     return { txt: '', minSeverity: 0 }
